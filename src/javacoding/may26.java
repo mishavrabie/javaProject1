c171ff69c104f32789d1c1c942329a531e593911
Mihail Vrabie <mishavrabie@yahoo.com>
2023-10-10 17:20:29 -0400

    firs comit

---
+++ src/javacoding/may26.java
@@ -0,0 +1,53 @@
+package javacoding;
+
+public class may26 {
+    // create a main method
+    public static void main(String[] args) {
+        // print statement
+        System.out.println("hello world"); //sout
+        System.out.println("i am learning java"); //sout
+        System.out.println("how are u");
+
+
+        int age = 20;
+
+
+
+        byte num1 = 5;
+        short num2 = 234;
+        int num3 = 1234;
+        long num4 =12345L;
+
+        System.out.println(num1);
+        System.out.println("num3");
+
+        // decimals
+        float num5 = 4.5F;
+        double num6 =45.23;
+
+        System.out.println(num5);
+        System.out.println(num6);
+
+        //character
+        char a = '9';
+        System.out.println();
+
+        //boolean --> true/fals
+        boolean hasWindow = false;
+        System.out.println(hasWindow);
+
+        String name = "Anna";
+        String address = "301 street way";
+        System.out.println(name);
+        System.out.println("address");
+
+        // declering multiple variables
+
+       // int a;
+        int b;
+        int c;
+
+
+
+    }
+}
